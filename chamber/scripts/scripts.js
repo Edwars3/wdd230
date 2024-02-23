54fb6ce872047b9c7b7c801ac23f273ca0758904
Edwars3 <42347560+Edwars3@users.noreply.github.com>
2024-02-23 13:53:04 -0300

    dark_mode

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,12 +1,10 @@
-// getdates.js
-
 document.addEventListener('DOMContentLoaded', function () {
     // Get the current year
     const currentYear = new Date().getFullYear();
 
     // Update the first paragraph in the footer with the copyright year
     const copyrightParagraph = document.querySelector('footer p:first-child');
-    copyrightParagraph.textContent = `© ${currentYear} Edwars Rodriguez`;
+    copyrightParagraph.textContent = `© ${currentYear} Colonia Tovar Chamber`;
 
     // Get the last modified date of the document
     const lastModifiedDate = new Date(document.lastModified);
@@ -19,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     lastModifiedParagraph.textContent = `Last Modified: ${formattedLastModifiedDate}`;
 });
 
+
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('nav');
 
@@ -27,6 +26,18 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 });
 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
 
 
-
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
